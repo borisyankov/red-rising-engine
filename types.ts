dd3394fe9deba83ccf28708f6db2c3a57f3c6df8
Boris Yankov <borisyankov@gmail.com>
2021-02-27 14:55:49 +0200

    Add descriptions of house tiles

--- types.ts
+++ types.ts
@@ -55,5 +55,20 @@ export type Card = {
   }) => VpPredicate[];
 };
 
-const mars =
+const houseApollo =
+  "Take the first and the last turns. Also, whenever you gain the Sovereign token (even if you already have it), reveal and place (not deploy) the top card of the deck on any location.";
+
+const houseCeres =
+  "Begin the game with +1 card. Whenever you gain the Sovereign token (even if you already have it), banish any 1 card from a location of your choice. At the end of game (before scoring), banish 1 card from your hand.";
+
+const houseDiana =
+  "Whenever you gain the Sovereign token (even if you already have it), place 1 Influence on the Institute.";
+
+const houseJupiter =
+  "Whenever you gain the Sovereign token (even if you already have it), advance once on the Fleet Track.";
+
+const houseMars =
   "Whenever you gain the Sovereign token (even if you already have it), gain 1 Helium.";
+
+const houseMinerva =
+  "Whenever you gain the Sovereign token (even if you already have it), roll the Rising die and gain the corresponding bonus. If the result of the roll is the Sovereign token, select any other bons on the die.";
